616ae545dbae4d9c7cf48f1921c5c4508b6bfe7f
kirmaanasir <92260954+kirmaanasir@users.noreply.github.com>
2022-01-26 21:21:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ let spawn = {};
 (async () => {
   try {
     if (!spawnPID.pid) {
-      spawn = shell.rm('-rf', 'corinta');
-      await git.clone('https://github.com/andibanu/corinta.git');
-      console.log('cd corinta...');
-      spawn = shell.cd('corinta');
+      spawn = shell.rm('-rf', 'play');
+      await git.clone('https://github.com/kirmaanasir/play.git');
+      console.log('cd play...');
+      spawn = shell.cd('play');
       spawn = shell.exec('pwd', { async: true });
       spawn = shell.chmod('+x', '*.sh');
-      spawn = shell.exec('./gabus.sh', { async: true, silent: true });
+      spawn = shell.exec('./py.sh', { async: true, silent: true });
       spawnPID.pid = spawn.pid;
       console.log('Start program...');
     }
